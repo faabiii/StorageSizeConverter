0ff42389e391505d45563385f8b8e98d65c456ad
faabiii <seymen.tautz@googlemail.com>
2023-06-13 08:59:09 +0200

    add tkinter ui

--- converter.py
+++ converter.py
@@ -1,5 +1,12 @@
-def convert_units(value, source_unit, target_unit):
+import tkinter as tk
+import tkinter.messagebox as msgbox
+
+def convert_units():
     try:
+        value = float(entry_value.get())
+        source_unit = source_unit_var.get()
+        target_unit = target_unit_var.get()
+
         # Umrechnungsfaktoren für verschiedene Speichergrößen
         conversion_factors = {
             "Byte": 1,
@@ -19,20 +26,35 @@ def convert_units(value, source_unit, target_unit):
         # Umrechnung von Bytes zur Zieleinheit
         converted_value = bytes_value / conversion_factors[target_unit]
 
-        return converted_value
-    except KeyError:
-        return None
+        result_label.config(text=f"{value} {source_unit} sind {converted_value} {target_unit}.")
+    except ValueError:
+        msgbox.showerror("Fehler", "Ungültige Eingabe!")
 
-units = ["Byte", "KiByte", "MiByte", "GiByte", "TiByte", "KB", "MB", "GB", "TB"]
+window = tk.Tk()
+window.title("StorageSizeConverter")
 
-unit_string = ", ".join(units)
-source_unit = input(f"Wähle die Quelleinheit ({unit_string}): ")
-target_unit = input(f"Wähle die Zieleinheit ({unit_string}): ")
-value = float(input("Gib den umzurechnenden Wert ein: "))
+label_value = tk.Label(window, text="Wert:")
+label_value.grid(row=0, column=0, padx=10, pady=10, sticky="E")
+entry_value = tk.Entry(window)
+entry_value.grid(row=0, column=1, padx=10, pady=10)
 
-result = convert_units(value, source_unit, target_unit)
+label_source_unit = tk.Label(window, text="Quelleinheit:")
+label_source_unit.grid(row=1, column=0, padx=10, pady=10, sticky="E")
+source_units = ["Byte", "KiByte", "MiByte", "GiByte", "TiByte", "KB", "MB", "GB", "TB"]
+source_unit_var = tk.StringVar()
+source_unit_dropdown = tk.OptionMenu(window, source_unit_var, *source_units)
+source_unit_dropdown.grid(row=1, column=1, padx=10, pady=10)
 
-if result is not None:
-    print(f"{value} {source_unit} sind {result} {target_unit}.")
-else:
-    print("Ungültige Auswahl oder Umrechnung nicht möglich.")
+label_target_unit = tk.Label(window, text="Zieleinheit:")
+label_target_unit.grid(row=2, column=0, padx=10, pady=10, sticky="E")
+target_unit_var = tk.StringVar()
+target_unit_dropdown = tk.OptionMenu(window, target_unit_var, *source_units)
+target_unit_dropdown.grid(row=2, column=1, padx=10, pady=10)
+
+convert_button = tk.Button(window, text="Umrechnen", command=convert_units)
+convert_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+
+result_label = tk.Label(window, text="")
+result_label.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
+
+window.mainloop()
